c8d5dac7fc53eb9a8eee5fab82350187e165acda
nikhayes <nikolashayes@dhcp-10-102-58-41.wireless.northwestern.private>
2023-05-31 15:27:02 -0500

    Fixed dynamic versioning for distros

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,8 @@
-from importlib.metadata import version, PackageNotFoundError
+from ._version import version as __version__
 
-try:
-    __version__ = version("fibphoflow")
-except PackageNotFoundError:
-    # If the package is not installed, don't add __version__
-    pass
+#from importlib.metadata import version, PackageNotFoundError
 
-try:
-    from importlib.metadata import version, PackageNotFoundError
-except ImportError:
-    from importlib_metadata import version, PackageNotFoundError
+#try:
+#    from importlib.metadata import version, PackageNotFoundError
+#except ImportError:
+#    from importlib_metadata import version, PackageNotFoundError
